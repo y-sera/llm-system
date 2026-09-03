3ed1a44edf84b52f4e00f0e8f41efd207225d8e5
yasuaki sera <yasuaki.sera15@gmail.com>
2026-09-04 02:58:46 +0900

    modify create-hf-secret

--- jetson/create-hf-secret.sh
+++ jetson/create-hf-secret.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl create -n llm-system secret generic hf-token-secret --from-file=token=$HOME/hf_token_$(hostname)
+kubectl create -n llm-system secret generic hf-token-secret --from-file=token=$HOME/.cache/huggingface/token
