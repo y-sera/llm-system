0f7102770eeb854e40b2bbc2dd10ff8946701446
yasuaki sera <yasuaki.sera15@gmail.com>
2026-09-16 23:49:08 +0900

    modify k3s install script

--- jetson/install-k3s.sh
+++ jetson/install-k3s.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --node-ip 192.168.1.96 --node-external-ip 192.168.1.96
+NETWORK_INTERFACE="${NETWORK_INTERFACE:-enP8p1s0}"
+IP_ADDRESS="$(ip -4 -j addr show "${NETWORK_INTERFACE}" | jq '.[].addr_info.[].local' -r)"
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --node-ip "${IP_ADDRESS}" --node-external-ip "${IP_ADDRESS}"
